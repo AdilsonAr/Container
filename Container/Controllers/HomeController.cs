57f240347d038d0e889a83f9a81cb7f15034009f
AdilsonAr <cycles.250@gmail.com>
2021-05-15 23:12:27 -0600

    conexion y prueba basica de S3

--- Container/Controllers/HomeController.cs
+++ Container/Controllers/HomeController.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Container.FileRepository;
+using System.Threading.Tasks;
 
 namespace Container.Controllers
+
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult IndexParcial()
+        {
+            return View();
+        }
+        
+        public ActionResult LoginParcial()
+        {
+            return View();
+        }
+   
+        public async Task<JsonResult> Test()
+        {
+            FileRepo repo = new FileRepo();
+            string s = await repo.testListBucketsAsync();
+            return Json(s,JsonRequestBehavior.AllowGet);
         }
     }
 }
---
+++ Container/FileRepository/FileRepo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon;
+using Amazon.Runtime;
+using Container.Secrets;
+
+namespace Container.FileRepository
+{
+    public class FileRepo
+    {
+        public async Task<string> testListBucketsAsync()
+        {
+            string r = "";
+            var s3Client = new AmazonS3Client(S3.awsAccessKeyId,
+                S3.awsSecretAccessKey, S3.regionEndpoint);
+            var listResponse = await s3Client.ListBucketsAsync();
+            foreach (S3Bucket b in listResponse.Buckets)
+            {
+                r+=b.BucketName;
+            }           
+            return r;
+        }
+    }
+}
